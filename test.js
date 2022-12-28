4ec3f4616ab7c61e54659749e36b43b5dd25d92b
thevasanthram <thevasanthram@github.com>
2022-12-28 12:35:38 +0530

    forgot password otp feature added

--- test.js
+++ test.js
@@ -1,8 +1,27 @@
 // const secretkey = require('crypto').randomBytes(64).toString('hex');
 // console.log(secretkey);
+const nodemailer = require('nodemailer');
 
-if (true) {
-  console.log('triggered');
-} else {
-  console.log('else');
-}
+let mailTransporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'vasanthram227@gmail.com',
+    pass: 'qpmexclhxkaricjo',
+  },
+});
+
+let mailDetails = {
+  from: 'vasanthram227@gmail.com',
+  to: 'manvendrasrmcem.1996@gmail.com',
+  subject: 'Test mail',
+  text: 'testing mail from NodeJS',
+};
+
+mailTransporter.sendMail(mailDetails, function (err, data) {
+  if (err) {
+    console.log('Error Occurs');
+    console.log(err);
+  } else {
+    console.log('Email sent successfully');
+  }
+});
